49f719dca458f33faea06ea9b1d9b0b8e4b0cb6e
Shraddha Yeole <shraddhayeole@Shraddhas-MacBook-Pro.local>
2018-12-08 02:51:48 -0800

    Add files

---
+++ mastersproject/extractiveSummary/nlp/tokenizers.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+import re
+import zipfile
+import nltk
+
+from .._compat import to_string, to_unicode, unicode
+from ..utils import normalize_language
+
+
+class DefaultWordTokenizer(object):
+    def tokenize(self, text):
+        return nltk.word_tokenize(text)
+
+
+class Tokenizer(object):
+
+
+    _WORD_PATTERN = re.compile(r"^[^\W\d_]+$", re.UNICODE)
+   
+    LANGUAGE_ALIASES = {
+        "slovak": "czech",
+    }
+
+    # improve tokenizer by adding specific abbreviations it has issues with
+    # note the final point in these items must not be included
+    LANGUAGE_EXTRA_ABREVS = {
+        "english": ["e.g", "al", "i.e"],
+        "german": ["al", "z.B", "Inc", "engl", "z. B", "vgl", "lat", "bzw", "S"],
+    }
+
+   
+
+    def __init__(self, language):
+        language = normalize_language(language)
+        self._language = language
+
+        tokenizer_language = self.LANGUAGE_ALIASES.get(language, language)
+        self._sentence_tokenizer = self._get_sentence_tokenizer(tokenizer_language)
+        self._word_tokenizer = self._get_word_tokenizer(tokenizer_language)
+
+    @property
+    def language(self):
+        return self._language
+
+    def _get_sentence_tokenizer(self, language):
+        try:
+            path = to_string("tokenizers/punkt/%s.pickle") % to_string(language)
+            return nltk.data.load(path)
+        except (LookupError, zipfile.BadZipfile):
+            raise LookupError(
+                "NLTK tokenizers are missing. Download them by following command: "
+                '''python -c "import nltk; nltk.download('punkt')"'''
+            )
+
+    def _get_word_tokenizer(self, language):
+            return DefaultWordTokenizer()
+
+    def to_sentences(self, paragraph):
+        if hasattr(self._sentence_tokenizer, '_params'):
+            extra_abbreviations = self.LANGUAGE_EXTRA_ABREVS.get(self._language, [])
+            self._sentence_tokenizer._params.abbrev_types.update(extra_abbreviations)
+        sentences = self._sentence_tokenizer.tokenize(to_unicode(paragraph))
+        return tuple(map(unicode.strip, sentences))
+
+    def to_words(self, sentence):
+        words = self._word_tokenizer.tokenize(to_unicode(sentence))
+        return tuple(filter(self._is_word, words))
+
+    def _is_word(self, word):
+        return bool(Tokenizer._WORD_PATTERN.search(word))
---
+++ mastersproject/mastersproject/views.py
@@ -0,0 +1,36 @@
+# Make Python 2/3 compatible codebase
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from extractiveSummary.parser.html import HtmlParser
+from extractiveSummary.nlp.tokenizers import Tokenizer
+from extractiveSummary.summarizer.lsa import LsaSummarizer as Summarizer
+from extractiveSummary.nlp.stemmers import Stemmer
+from extractiveSummary.utils import get_stop_words
+
+
+LANGUAGE = "english"
+
+
+
+def homepage(request):
+	return render(request, 'home.html')
+
+def count(request):
+	URL = request.GET.get('urlvalue')
+	SENTENCES_COUNT = request.GET.get('SENTENCES_COUNT')
+	parser = HtmlParser.from_url(URL, Tokenizer(LANGUAGE))
+	stemmer = Stemmer(LANGUAGE)
+	summarizer = Summarizer(stemmer)
+	summarizer.stop_words = get_stop_words(LANGUAGE)
+	result = []
+	for sentence in summarizer(parser.document, SENTENCES_COUNT):
+    		result.append(sentence)
+
+		
+	return render(request, 'count.html', {'result': result})
+	
